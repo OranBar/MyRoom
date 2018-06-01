bd5bf17e5766a37071c96ae02a23471985569080
Oran <baroran@hotmail.com>
2018-06-01 15:10:17 +0200

    Added android vr and camera hotspots

---
+++ Assets/HoverDemos/Common/DemoAdjustInputSceneCamera.cs
@@ -0,0 +1,27 @@
+﻿using Hover.Core.Utils;
+using UnityEngine;
+
+namespace HoverDemos.Common {
+
+	/*================================================================================================*/
+	public class DemoAdjustInputSceneCamera : HoverSceneAdjust {
+
+		public string CameraName;
+		public string MainSceneCameraName = "MainCamera";
+		public bool UseBloom = true;
+
+
+		////////////////////////////////////////////////////////////////////////////////////////////////
+		/*--------------------------------------------------------------------------------------------*/
+		protected override bool IsReadyToAdjust() {
+			return (GameObject.Find(CameraName) != null);
+		}
+
+		/*--------------------------------------------------------------------------------------------*/
+		protected override void PerformAdjustments() {
+			
+		}
+
+	}
+
+}
---
+++ Assets/Programming/Oran/Scripts/CameraSpotlight/CameraSpotlight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pixelplacement;
+using NaughtyAttributes;
+
+public class CameraSpotlight : MonoBehaviour {
+
+	public float startDelay;
+	public float transitionDuration = 2.2f;
+
+	[Button]
+	public void MoveMainCameraToSpot() {
+        Tween.Position(Camera.main.transform.parent, this.transform.position, transitionDuration, startDelay);
+		Tween.Rotation(Camera.main.transform.parent, this.transform.rotation, transitionDuration, startDelay);
+		//Camera.main.transform.position = this.transform.position;
+		//Camera.main.transform.rotation = this.transform.rotation;
+	}
+}
