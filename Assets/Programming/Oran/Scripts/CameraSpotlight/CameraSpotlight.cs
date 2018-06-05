bcaa7a129f52fa79c7dcf27e42e8dc4f65380b28
Oran <baroran@hotmail.com>
2018-06-05 14:27:47 +0200

    Refactor
    and minor improvements in scenes

--- Assets/Programming/Oran/Scripts/CameraSpotlight/CameraSpotlight.cs
+++ Assets/Programming/Oran/Scripts/CameraSpotlight/CameraSpotlight.cs
@@ -13,29 +13,45 @@ public class CameraSpotlight : MonoBehaviour {
 	public OBEvent onTransitionFinish;
 
 	[Button]
-	public void MoveMainCameraToSpot() {
-        VRForegroundCanvas.Instance.FadeIn(transitionDuration/3);
-		var prevNearClipPlane_value = Camera.main.nearClipPlane;
-		Camera.main.nearClipPlane = 0.001f;
-		
-		//Fade out black canvas
-		Camera.main.gameObject.ExecuteDelayed(
-			()=> {
-				VRForegroundCanvas.Instance.FadeOut(transitionDuration/3);
-			}
-		,(transitionDuration/3f)*2.5f);
-
-		//Fix clipping plane. This will probably make the black canvas disappear, so we do it at the end.
-		Camera.main.gameObject.ExecuteDelayed((	)=> {
-				Camera.main.nearClipPlane = prevNearClipPlane_value;
-			}
-		,transitionDuration);
-		
-		Tween.Position(Camera.main.transform.parent, this.transform.position, transitionDuration, startDelay);
-		Tween.Rotation(Camera.main.transform.parent, this.transform.rotation, transitionDuration, startDelay, completeCallback: ()=>onTransitionFinish.Invoke() );
+	public void MoveMainCameraToSpot()
+    {
+		MoveMainCameraToSpot(transitionDuration);
 	}
 
-	public RenderTexture GetCurrentShot(int width = 512, int height = 512){
+	public void MoveMainCameraToSpot(float animDduration)
+    {
+
+        Tween.Position(Camera.main.transform.parent, this.transform.position, animDduration, startDelay);
+        Tween.Rotation(Camera.main.transform.parent, this.transform.rotation, animDduration, startDelay, completeCallback: () => onTransitionFinish.Invoke());
+        FadeInAndOut_BlackCanvas(animDduration);
+
+    }
+
+    private void FadeInAndOut_BlackCanvas(float animDduration)
+    {
+        //Make sure the canvas is shown
+        var prevNearClipPlane_value = Camera.main.nearClipPlane;
+        Camera.main.nearClipPlane = 0.001f;
+
+        VRForegroundCanvas.Instance.FadeIn(animDduration / 3);
+
+        //Fade out black canvas
+        Camera.main.gameObject.ExecuteDelayed(
+            () =>
+            {
+                VRForegroundCanvas.Instance.FadeOut(animDduration / 3);
+            }
+        , (animDduration / 3f) * 2.5f);
+
+        //Fix clipping plane. This will probably make the black canvas disappear, so we do it at the end.
+        Camera.main.gameObject.ExecuteDelayed(() =>
+        {
+            Camera.main.nearClipPlane = prevNearClipPlane_value;
+        }
+        , animDduration);
+    }
+
+    public RenderTexture GetCurrentShot(int width = 512, int height = 512){
 		RenderTexture result = new RenderTexture (width, height, 16);
 		result.name = this.name+"_rt";
         Camera camera1 = GetComponent<Camera>();
