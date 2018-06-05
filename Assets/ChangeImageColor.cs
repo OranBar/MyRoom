f3e31d0b05ef9eb54b7a8328a25f76846fcf439f
Oran <baroran@hotmail.com>
2018-06-05 19:27:31 +0200

    Room 14 now has paintings, rug, and billboard

---
+++ Assets/ChangeImageColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Hover.RendererModules.Alpha;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeHoverColor : MonoBehaviour {
+
+	private HoverAlphaMeshUpdater myImage;
+	[ColorUsage(true, true, 0, 1000, 0, 1000)]
+	public Color targetColor;
+
+	public void Awake(){
+		myImage = GetComponent<HoverAlphaMeshUpdater>();
+	}
+
+	public void ChangeColorToTarget(){
+		myImage.StandardColor = targetColor;
+	}
+	
+	
+}
--- Assets/Programming/Oran/Scripts/ExtensionMethods/MonoBehaviourEx.cs
+++ Assets/Programming/Oran/Scripts/ExtensionMethods/MonoBehaviourEx.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Assertions;
 using System.Linq;
+using AdvancedCoroutines;
 
 namespace OranUnityUtils
 {
